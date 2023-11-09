4e4d3fe6c8480ef531137f2ecaf639a43113b3f3
deepblue597 <iasonas2001@hotmail.com>
2023-11-10 00:48:28 +0200

    added losses

--- multilayer-perceptron.py
+++ multilayer-perceptron.py
@@ -49,12 +49,31 @@ class Activation_Softmax:
         self.output = probabilites
 
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:  # one hot encoded
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
 # to prevent overflow we (values too big to calculate) what we do is we subtract the max value from the inputs
 # now the values are between zero and 1.
 # then we divide by the sum so we have our probabilites
 
 
-# TODO: check if i the neurons are [n,1] or [1,n]
 # def layer_function(inputs , weights , bias):
 
 #     layer = [] #initialize the neurons
@@ -111,3 +130,50 @@ layer_2.forward(activation_1.output)
 
 activation_2.forward(layer_2.output)
 print(activation_2.output[:5])
+
+# =============================================================================
+# Mean Squared Error (MSE):
+#
+# Used for regression problems where the target variable is a continuous value.
+# The loss is calculated as the square of the difference between the predicted value (a) and the actual target value (y): (a - y)^2.
+# MSE is often used when the output of the MLP represents a single continuous value, and you want to measure the difference between the predicted and actual values.
+#
+# Categorical Cross-Entropy:
+#
+# Used for classification problems where the target variable is a categorical variable (e.g., one-hot encoded class labels).
+# The loss is calculated based on the predicted class probabilities (a) and the actual class labels (y). It measures the dissimilarity between the predicted and actual class distributions using the cross-entropy formula.
+# Categorical cross-entropy is commonly used when the output of the MLP represents class probabilities, and you want to measure the difference between the predicted class distribution and the true class distribution.
+# =============================================================================
+# log in python is ln
+
+# %%
+
+
+# print(np.log(np.e))
+# # y = np.array([[1, 0 , 0 ],
+# #      [0, 1 , 0],
+# #      [0, 0 , 1]])
+
+# y_pred = np.array([[0.66 , 0.33 , 0.01],
+#           [0.9 , 0.05 , 0.05],
+#           [0.2 , 0.2 , 0.6]])
+
+# class_target =[ 0 , 1 , 2]
+# loss = - np.sum(y * np.log(y_pred), axis=1 , keepdims=True)
+
+
+# print(y_pred[range(len(y_pred)),class_target])
+
+# y_new = y_pred[range(len(y_pred)),class_target]
+# loss_new = - np.log(y_new) #this is correct. we take the correct classifications with the predictions and we create the values
+
+# if a prediction is completely wron (pred = 0 ) then the lg is inf
+# to prevent that we use clip
+# print(1e-7)
+# loss_new = np.clip(loss_new, 1e-7, 1 - 1e-7)
+
+# %%
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation_2.output, y)
+print("Loss:", loss)
