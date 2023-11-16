96488c655e8028f3ab62e3489b92833bf6eb49a7
deepblue597 <iasonas2001@hotmail.com>
2023-11-16 17:13:54 +0200

    minor fixes

--- final.py
+++ final.py
@@ -27,9 +27,7 @@ nnfs.init()
 class Layer_Dense:
 
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons,
-                 weight_regularizer_l1=0, weight_regularizer_l2=0,
-                 bias_regularizer_l1=0, bias_regularizer_l2=0):
+    def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
