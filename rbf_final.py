e075d65c63fe1fba7ff7eda768b3bb3944c18bfb
deepblue597 <iasonas2001@hotmail.com>
2024-01-04 01:36:40 +0200

    created py_torch compatible classes (but less accuracy)

--- rbf_final.py
+++ rbf_final.py
@@ -17,6 +17,10 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sns # for statistical data visualization
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
 
 #%% Gaussin class 
 
@@ -70,6 +74,79 @@ class RBFNeuralNetwork(GaussianRBF, CentersInitializer):
 
         # Make predictions using logistic regression
         return self.logistic_regression.predict(rbf_outputs_test)
+
+#%% 
+
+class GaussianRBF_torch:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def gaussian_rbf(x, center, sigma):
+        return torch.exp(-torch.cdist(x, center, p=2) / (2 * sigma**2))
+
+
+class RBFNeuralNetwork_torch(GaussianRBF_torch, CentersInitializer):
+    def __init__(self, n_centers, rbf_width):
+        self.n_centers = n_centers
+        self.rbf_width = rbf_width
+        self.rbf_centers = None
+        self.logistic_regression = None
+
+    def fit(self, X_train, y_train, center_initializer='random', lr=0.01, epochs=100 , weight_decay = 1e-3 , optimizer = 'adam'):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        if center_initializer == 'random':
+            self.rbf_centers = torch.tensor(self.random_centers(n_centers=self.n_centers, X_train=X_train),
+                                            dtype=torch.float32, device=device)
+        elif center_initializer == 'k_means':
+            self.rbf_centers = torch.tensor(self.k_means_centers(X_train, max_iters=300, n_centers=self.n_centers),
+                                            dtype=torch.float32, device=device)
+
+        rbf_outputs_train = self.gaussian_rbf(torch.tensor(X_train, dtype=torch.float32, device=device),
+                                              self.rbf_centers, self.rbf_width)
+
+        input_dim = rbf_outputs_train.shape[1]
+        output_dim = len(np.unique(y_train))
+
+        model = nn.Linear(input_dim, output_dim)
+        criterion = nn.CrossEntropyLoss()
+        if optimizer == 'adam': 
+            optimizer = optim.Adam(model.parameters(), lr=lr , weight_decay= weight_decay)
+        elif optimizer == 'sgd' : 
+            optimizer = optim.SGD(model.parameters(), lr=lr)
+
+        X_train_tensor = torch.tensor(X_train, dtype=torch.float32, device=device)
+        y_train_tensor = torch.tensor(y_train, dtype=torch.long, device=device)
+
+        dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+        for epoch in range(epochs):
+            for inputs, labels in dataloader:
+                optimizer.zero_grad()
+                rbf_outputs = self.gaussian_rbf(inputs, self.rbf_centers, self.rbf_width)
+                logits = model(rbf_outputs)
+                loss = criterion(logits, labels)
+                loss.backward()
+                optimizer.step()
+
+            if epoch % 10 == 0:
+                with torch.no_grad():
+                    rbf_outputs_test = self.gaussian_rbf(torch.tensor(X_train, dtype=torch.float32, device=device),
+                                                         self.rbf_centers, self.rbf_width)
+                    logits_test = model(rbf_outputs_test)
+                    y_pred = torch.argmax(logits_test, dim=1).cpu().numpy()
+                    accuracy = accuracy_score(y_train, y_pred)
+                    print(f"Epoch {epoch}, Accuracy: {accuracy * 100:.2f}%")
+
+        self.logistic_regression = model
+
+    def predict(self, X_test):
+        rbf_outputs_test = self.gaussian_rbf(torch.tensor(X_test, dtype=torch.float32),
+                                             self.rbf_centers, self.rbf_width)
+        logits = self.logistic_regression(rbf_outputs_test)
+        return torch.argmax(logits, dim=1).cpu().numpy()
 
 
 #%% 
@@ -140,12 +217,20 @@ batch_size_test = 2000
 X_batch_test = X_test[:batch_size_test]
 y_batch_test = y_test[:batch_size_test]
 
-#%% 
+#%% custom made rbf nn 
+
 centers = [10 , 50 , 100 , 200 , 500]
 variance = [ 0.5 , 1 , 5 , 8 , 10 , 20 ]
 
 rbf_nn = RBFNeuralNetwork(n_centers=500, rbf_width=10.0)
 rbf_nn.fit(X_train, y_train, center_initializer='random')
+
+#%% pytorch
+
+rbf_nn_tensor = RBFNeuralNetwork_torch(n_centers=100, rbf_width=8.0)
+rbf_nn_tensor.fit(X_train, y_train, center_initializer='random', lr=0.05, epochs=100 , optimizer='adam')
+
+#%% predictions
 
 # Make predictions on test set
 y_pred = rbf_nn.predict(X_test)
